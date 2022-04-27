b04979d2626f11a09592c612ca83f3d51e8d7433
rickyriosp <riki.rios91@gmail.com>
2022-04-27 13:47:56 -0400

    updated controller to use dtos

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
-﻿using Commander.Models;
+﻿using AutoMapper;
+using Commander.DTOs;
+using Commander.Models;
 using Commander.Services.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,28 +12,42 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICmdrRepository _repository;
+        private readonly IMapper _mapper;
 
-        public CommandsController(ICmdrRepository repository)
+        public CommandsController(ICmdrRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         // GET api/commands
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Command>>> GetAllCommands()
+        public async Task<ActionResult<IEnumerable<CommandReadDto>>> GetAllCommands()
         {
             var commandItems = await _repository.GetAllCommandsAsync();
+            if (commandItems == null)
+            {
+                return NotFound();
+            }
 
-            return Ok(commandItems);
+            var commandDtos = _mapper.Map<IEnumerable<CommandReadDto>>(commandItems);
+
+            return Ok(commandDtos);
         }
 
         // GET api/commands/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Command>> GetCommandById(int id)
+        public async Task<ActionResult<CommandReadDto>> GetCommandById(int id)
         {
             var commandItem = await _repository.GetCommandByIdAsync(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
 
-            return Ok(commandItem);
+            var commandDto = _mapper.Map<CommandReadDto>(commandItem);
+
+            return Ok(commandDto);
         }
     }
 }
