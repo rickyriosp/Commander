99e366dc23cbcf60fb491a45b42fb077b11456b3
rickyriosp <riki.rios91@gmail.com>
2022-04-27 10:47:35 -0400

    commands controller get actions

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,37 @@
+﻿using Commander.Models;
+using Commander.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICRepository _repository;
+
+        public CommandsController(ICRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
--- Commander/Services/MockCRepository.cs
+++ Commander/Services/MockCRepository.cs
@@ -10,8 +10,8 @@ namespace Commander.Services
             var commands = new List<Command>()
             {
                 new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan" },
-                new Command { Id = 0, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & Chopping Board" },
-                new Command { Id = 0, HowTo = "Make a cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" }
+                new Command { Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife & Chopping Board" },
+                new Command { Id = 2, HowTo = "Make a cup of tea", Line = "Place teabag in cup", Platform = "Kettle & Cup" }
             };
 
             return commands;
