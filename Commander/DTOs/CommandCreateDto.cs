9dccb3f8f315677e5feebe697fdee0c931d2eeb5
rickyriosp <riki.rios91@gmail.com>
2022-04-27 14:39:53 -0400

    added data annotations on CommandCreateDto

--- Commander/DTOs/CommandCreateDto.cs
+++ Commander/DTOs/CommandCreateDto.cs
@@ -1,9 +1,19 @@
-﻿namespace Commander.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Commander.DTOs
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Line { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Platform { get; set; }
     }
 }
